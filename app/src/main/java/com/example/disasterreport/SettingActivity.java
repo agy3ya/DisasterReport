a79927b43922e8f4dc90528cbb22b7dce3ba1a10
Ankit-Singh <xyelblaze@gmail.com>
2020-12-25 01:54:04 +0530

    Add order by preference

--- app/src/main/java/com/example/disasterreport/SettingActivity.java
+++ app/src/main/java/com/example/disasterreport/SettingActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -24,6 +25,8 @@ public class SettingActivity extends AppCompatActivity {
             addPreferencesFromResource(R.xml.settings_main);
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
 
         }
 
@@ -31,14 +34,25 @@ public class SettingActivity extends AppCompatActivity {
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
             String stringValue = newValue.toString();
-            preference.setSummary(stringValue);
+            if(preference instanceof ListPreference){
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if(prefIndex>=0){
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            }
+            else {
+                preference.setSummary(stringValue);
+            }
+
             return true;
         }
-        private void bindPreferenceSummaryToValue(Preference minMagnitude) {
-            minMagnitude.setOnPreferenceChangeListener(this);
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(minMagnitude.getContext());
-            String preferenceString = preferences.getString(minMagnitude.getKey(),"");
-            onPreferenceChange(minMagnitude,preferenceString);
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(),"");
+            onPreferenceChange(preference,preferenceString);
         }
     }
 }
