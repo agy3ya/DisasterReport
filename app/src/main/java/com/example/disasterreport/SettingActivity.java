a0140e31058792ca66713d2ccfd9c71fd340e0dc
Ankit-Singh <xyelblaze@gmail.com>
2020-12-25 01:34:35 +0530

    Add OnPreferenceChangeListener

--- app/src/main/java/com/example/disasterreport/SettingActivity.java
+++ app/src/main/java/com/example/disasterreport/SettingActivity.java
@@ -3,8 +3,11 @@ package com.example.disasterreport;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -13,12 +16,29 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
     }
-    public static class EarthquakePreferenceFragment extends PreferenceFragment{
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+
+        }
+
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+        private void bindPreferenceSummaryToValue(Preference minMagnitude) {
+            minMagnitude.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(minMagnitude.getContext());
+            String preferenceString = preferences.getString(minMagnitude.getKey(),"");
+            onPreferenceChange(minMagnitude,preferenceString);
         }
     }
 }
